a90fd4357659c9199192d78677ed9bb5fd9deb34
bodroghykristof <bodroghykristof@gmail.com>
2020-12-07 15:14:58 +0100

    Change function-based to class-based components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import { Component } from 'react';
 import './App.css';
+import Todos from './components/Todos';
 
-function App() {
-  return (
-    <div className='App'>
-      <h1>App</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <Todos />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Todos.js
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+import '../App.css';
+
+class Todos extends Component {
+  redner() {
+    return (
+      <div>
+        <h1>Todos</h1>
+      </div>
+    );
+  }
+}
+
+export default Todos;
