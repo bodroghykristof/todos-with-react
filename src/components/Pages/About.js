362867f0584bbe6e0f520d83c2a398a0ffa8df8a
bodroghykristof <bodroghykristof@gmail.com>
2020-12-07 20:56:39 +0100

    Add About.js

---
+++ src/components/Pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>
+        TODOs delivered by Bodroghy Kristóf. Developed in React on 7th Dec. 2020
+      </p>
+    </React.Fragment>
+  );
+}
